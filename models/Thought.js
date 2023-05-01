bc37471ca0a5f1009f3b1081478fd47360856b0b
Hunter Wilson <huntertheguy123@gmail.com>
2023-05-01 18:03:12 -0500

    add to connection.js and made models for Thought and User

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+           type: String,
+           require: true,
+           minLength: 1,
+           maxLength: 280 
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //add getter method here
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: [
+            {
+                type: ,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: ,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+const User = model('User', userSchema)
+
+module.exports = User;
